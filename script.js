c14c1c447491c051edb52f8423e24c77ebfee726
arfrhmnshrtn <iw8628940@gmail.com>
2021-07-10 13:09:45 +0700

    menambahkan dua opsi pilihan, luas dan keliling

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 const input = document.querySelector('.input');
+const hasil = document.querySelector('.hasil');
 const button = document.querySelector('.button');
 const check = document.querySelector('.check');
-const option = document.getElementById('select');
-
-function show(){
-    var as = document.forms[0].select.value;
-    var strUser = option.options[option.selectedIndex].value;
-    console.log(as, strUser);
-}
-option.onchange=show;
-show();
+const select = document.getElementById('select');
 
 let r = input.value;
 button.addEventListener('click', function(){
-    let r = input.value;
+    let r = Number(input.value);
+    let strUser = select.options[select.selectedIndex].value;
     if (r == ''){
         check.style.display = 'block';
     }else{
-        alert(`${22/7 * r**2} CM`);
-        input.value = '';
+        if (strUser === 'Luas Lingkaran'){
+            hasil.value = Math.trunc(22/7 * r**2) + ' CM';
+        }else{
+            hasil.value = Math.trunc(2 * 22/7 * r) + ' CM';
+        }
     }
 });
-
-
